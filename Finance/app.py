cd7937f8368824f67bb1fc71e9351d2bec401ba9
Arpit R <108040880+ArkTrek@users.noreply.github.com>
2024-07-25 13:29:07 +0530

    Add files via upload

---
+++ Finance/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/transactions')
+def transactions():
+    return render_template('transaction.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
